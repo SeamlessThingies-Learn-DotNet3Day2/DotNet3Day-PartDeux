7c0ce52e7851402c8318bfb17844178a4038420c
mheydt <mike@heydt.org>
2015-04-09 09:34:56 -0600

    Added Linq2Sql example

---
+++ St.Eg.Day3.M2.Ex3.Linq2SQL/DebugWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace St.Eg.Day3.M2.Ex3.Linq2SQL
+{
+    class DebugTextWriter : System.IO.TextWriter
+    {
+        public override void Write(char[] buffer, int index, int count)
+        {
+            System.Console.Write(new String(buffer, index, count));
+        }
+
+        public override void Write(string value)
+        {
+            System.Console.Write(value);
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.Default; }
+        }
+    }
+}
---
+++ St.Eg.Day3.M2.Ex3.Linq2SQL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace St.Eg.Day3.M2.Ex3.Linq2SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
